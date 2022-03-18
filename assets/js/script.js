a6404518af2352f0c76e6dc0359269a47ba4c9f2
jennj <jennjenn93@gmail.com>
2022-03-18 16:24:44 -0500

    display city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city-input");
 var weatherContainerEl = document.querySelector("#current-weather-container");
 var weatherSearchTerm = document.querySelector("#weather-search-term");
+var currentCityEl = document.querySelector("#current-city");
 
 
 var formSubmitHandler = function(event) {
@@ -26,16 +27,20 @@ var getWeatherData = function(cityName) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-           console.log(data, cityName);
+           //console.log(data, cityName);
 
            var longitude=data[0].lon;
            var latitude=data[0].lat;
+
+           var currentCityName =data[0].name;    
+            currentCityEl.textContent =currentCityName;
+            console.log(data);
 
            var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&appid=fe7326fd08b73adb3c80827fa94555ff&units=imperial";
         
            fetch(apiUrl2).then(function(response) {
                response.json().then(function(data) {
-                   console.log(data);
+                  displayCurrentWeather(data);
                })
         });
         
@@ -48,11 +53,8 @@ cityFormEl.addEventListener("submit", formSubmitHandler);
 
 
 
-var displayCurrentWeather = function() {
+//var displayCurrentWeather = function(data) {
     
-    var currentCityName =data[0].name;    
-    document.getElementById("#current-city").innerHTML = currentCityName;
-    console.log(currentCityName);
 
  
-};
+//};
